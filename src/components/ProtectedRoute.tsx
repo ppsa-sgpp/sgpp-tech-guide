fbd1316cc3387ed3b1de6f35eb8eb2995c716f2b
EduMelo <esmelo@stefanini.com>
2024-12-02 10:14:33 -0300

    corrigido uso de localstorage

--- src/components/ProtectedRoute.tsx
+++ src/components/ProtectedRoute.tsx
@@ -1,14 +1,19 @@
 import React from 'react';
+import BrowserOnly from '@docusaurus/BrowserOnly';
 import { Redirect } from '@docusaurus/router';
 
-const ProtectedRoute: React.FC<{ children: React.ReactNode }> = ({ children }) => {
-  const isAuthenticated = localStorage.getItem('authenticated') === 'true';
-
-  if (!isAuthenticated) {
-    return <Redirect to="/sgpp-tech-guide/login" />;
-  }
-
-  return <>{children}</>;
-};
+function ProtectedRoute({ children }: { children: React.ReactNode }) {
+  return (
+    <BrowserOnly fallback={<div>Carregando...</div>}>
+      {() => {
+        const isAuthenticated = localStorage.getItem('authenticated');
+        if (!isAuthenticated) {
+          return <Redirect to="/sgpp-tech-guide/login" />;
+        }
+        return <>{children}</>;
+      }}
+    </BrowserOnly>
+  );
+}
 
 export default ProtectedRoute;
