98fde9c51077892c6e5fa3da492ec5d3e2799eb1
ppsa-sgpp <ppsa-sgpp@users.noreply.github.com>
2024-12-02 13:15:27 +0000

    deploy: fbd1316cc3387ed3b1de6f35eb8eb2995c716f2b

--- assets/js/1df93b7f.23cd1f73.js
+++
@@ -1,1 +0,0 @@
-"use strict";(self.webpackChunksgpp_tech_guide=self.webpackChunksgpp_tech_guide||[]).push([[4583],{8198:(e,s,t)=>{t.r(s),t.d(s,{default:()=>c});t(4586),t(1107);var o=t(6347),r=t(6025);var a=t(4848);function c(){const e=(0,r.Ay)("/docs/intro");return console.log("Base URL utilizado para redirecionamento:",e),(0,a.jsx)(o.rd,{to:e})}}}]);
