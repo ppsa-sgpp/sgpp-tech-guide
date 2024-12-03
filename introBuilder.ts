90932dcf0d2ec409d93d7bd559a929a117ec45d4
EduMelo <esmelo@stefanini.com>
2024-12-03 13:51:04 -0300

    corrigida geração de página de intro

--- introBuilder.ts
+++ introBuilder.ts
@@ -20,34 +20,30 @@ function capitalizeWords(str: string): string {
 }
 
 // Função para construir o Markdown
-function buildMarkdown(dirPath: string, basePath = '', indent = ''): string {
+function buildMarkdown(dirPath: string, basePath = '', level = 3): string {
   const entries = fs.readdirSync(dirPath, { withFileTypes: true });
 
   // Processa os arquivos na raiz
   const docsInRoot = entries
-    .filter((entry) => entry.isFile())
-    .filter((entry) => entry.name.endsWith('.md'))
-    .filter((entry) => entry.name !== 'intro.md')
-    .map((entry) => {
-      console.log(entry.name)
+    .filter(entry => entry.isFile() && entry.name.endsWith('.md') && entry.name !== 'intro.md')
+    .map(entry => {
       const relativePath = path.join(basePath, entry.name);
       const fileName = entry.name.replace(/\.md$/, '');
-      return `${indent}-   [${capitalizeWords(fileName.replace(/-/g, ' '))}](docs/${relativePath})`;
+      return `- [${capitalizeWords(fileName.replace(/-/g, ' '))}](docs/${relativePath})`;
     });
-    console.log
 
   // Processa as subcategorias
   const categories = entries
-    .filter((entry) => entry.isDirectory())
-    .filter((entry => entry.name !== 'img'))
-    .map((entry) => {
+    .filter(entry => entry.isDirectory() && entry.name !== 'img')
+    .map(entry => {
       const fullPath = path.join(dirPath, entry.name);
       const relativePath = path.join(basePath, entry.name);
 
-      const items = buildMarkdown(fullPath, relativePath, `${indent}    `);
+      const items = buildMarkdown(fullPath, relativePath, level + 1);
 
       if (items) {
-        return `${indent}#### ${capitalizeWords(entry.name.replace(/-/g, ' '))}\n${items}`;
+        const header = `${'#'.repeat(level)} ${capitalizeWords(entry.name.replace(/-/g, ' '))}`;
+        return `${header}\n${items}`;
       }
       return null;
     })
@@ -68,9 +64,8 @@ sidebar_position: 1
 # Documentação
 <!-- Documentação da arquitetura e padrões de desenvolvimento do sistema SGPP. -->
 
-### Elementos de Construção
 ${sidebarContent}`;
 
-const outputPath = path.resolve(__dirname, 'docs', 'intro.md');
-fs.writeFileSync(outputPath, intro, 'utf8');
+  const outputPath = path.resolve(__dirname, 'docs', 'intro.md');
+  fs.writeFileSync(outputPath, intro, 'utf8');
 }
